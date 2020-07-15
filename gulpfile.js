8bb3583e12689961b701967472e870b5a5e8e1fa
Pablo <cruzpablomartin@gmail.com>
2020-07-14 21:16:29 -0300

    Actualización

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,55 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync');
 
-gulp.task('sass', gulp.series( () => {
-   return gulp.src([
-      'node_modules/bootstrap/scss/bootstrap.scss',
-      'src/scss/*.scss'
-   ])
-   .pipe(sass({outputStyle: 'compressed'}))
-   .pipe(gulp.dest('src/css'))
-   .pipe(browserSync.stream());
-}));
 
-gulp.task('js', gulp.series( () => {
+// BOOTSTRAP
+gulp.task('bootstrap-css', gulp.series( () => {
+   return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+      .pipe(gulp.dest('src/css'));
+}));
+gulp.task('bootstrap-js', gulp.series( () => {
    return gulp.src([
-      'node_modules/bootstrap/dist/js/bootstrap.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
       'node_modules/jquery/dist/jquery.min.js',
       'node_modules/popper.js/dist/umd/popper.min.js'
    ])
-   .pipe(gulp.dest('src/js'))
-   .pipe(browserSync.stream());
+      .pipe(gulp.dest('src/js'));
 }));
 
-gulp.task('font-awesome', gulp.series( () => {
+
+// FONT-AWESOME
+gulp.task('font-awesome-css', gulp.series( () => {
    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-   .pipe(gulp.dest('src/css'));
+      .pipe(gulp.dest('src/css'));
 }));
-
-gulp.task('fonts', gulp.series( () => {
+gulp.task('font-awesome-fonts', gulp.series( () => {
    return gulp.src('node_modules/font-awesome/fonts/*')
-   .pipe(gulp.dest('src/fonts'))
+      .pipe(gulp.dest('src/fonts'));
 }));
 
+
+// SASS COMPILER
+gulp.task('sass', gulp.series( () => {
+   return gulp.src('src/scss/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('src/css'))
+      .pipe(browserSync.stream());
+}));
+
+
+// STATIC SERVER + WATCHING SCSS/HTML FILES
 gulp.task('serve', gulp.series( ['sass'], () => {
    browserSync.init({
       server: {
          baseDir: './src'
-      }
+      },
+      notify: false
    });
+   gulp.watch('src/scss/**/*.scss', gulp.parallel( ['sass'] ));
+   gulp.watch('src/*.html').on('change', browserSync.reload);
 }));
-gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.parallel( ['sass'] ));
-gulp.watch(['src/*.html']).on('change', browserSync.reload);
 
-gulp.task('default', gulp.series(['font-awesome','fonts','js','serve']));
+gulp.task('default', gulp.series(['bootstrap-js', 'bootstrap-css', 'font-awesome-css', 'font-awesome-fonts', 'serve']));
